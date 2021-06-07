b9d0bc031bcd07ec22419198abce302ea209725f
Vitaliy Murashko <vitaliy884848@gmail.com>
2021-06-07 16:53:49 +0300

    base structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-function App() {
+import { Contacts } from './pages/Contacts/index'
+export function App() {
   return (
     <div className="App">
-      
+      <Contacts />
     </div>
   );
 }
 
-export default App;
+
