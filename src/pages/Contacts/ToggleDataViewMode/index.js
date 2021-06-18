36f42aabb5ae825c30b47ee228976feea30f7fe8
Vitaliy Murashko <vitaliy884848@gmail.com>
2021-06-19 00:28:23 +0300

    Add toggle view mode

---
+++ src/pages/Contacts/ToggleDataViewMode/index.js
@@ -0,0 +1,34 @@
+import { useCallback } from "react";
+import propTypes from "prop-types";
+import ViewListIcon from "@material-ui/icons/ViewList";
+import ViewModuleIcon from "@material-ui/icons/ViewModule";
+import ToggleButton from "@material-ui/lab/ToggleButton";
+import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
+import { DATA_VIEW_MODES } from '../constants';
+
+export const ToggleDataViewMode = ({dataViewMode, setDataViewMode}) => {
+
+    const handleChangeDataViewMode = useCallback((event, nextView) => {
+        setDataViewMode(nextView);
+      }, [setDataViewMode]);
+
+    return (
+        <ToggleButtonGroup
+              value={dataViewMode}
+              exclusive
+              onChange={handleChangeDataViewMode}
+        >
+              <ToggleButton value={DATA_VIEW_MODES.TABLE} aria-label={DATA_VIEW_MODES.TABLE}>
+                <ViewListIcon />
+              </ToggleButton>
+              <ToggleButton value={DATA_VIEW_MODES.GRID} aria-label={DATA_VIEW_MODES.GRID}>
+                <ViewModuleIcon />
+              </ToggleButton>
+         </ToggleButtonGroup>
+    );
+};
+
+ToggleDataViewMode.propTypes = {
+    dataViewMode:propTypes.oneOf([DATA_VIEW_MODES.TABLE, DATA_VIEW_MODES.GRID]).isRequired,
+    setDataViewMode:propTypes.func.isRequired  
+};
--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,48 +1,66 @@
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { useContacts } from './useContacts';
-import { ContactsTable } from './ContactsTable/index';
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Box from "@material-ui/core/Box";
+import { useContacts } from "./useContacts";
+import { ContactsTable } from "./ContactsTable/index";
+import { ToggleDataViewMode } from './ToggleDataViewMode/index';
+import { DATA_VIEW_MODES } from './constants';
+import { useDataViewMode } from './useDataViewMode';
 
-const useStyles = makeStyles((theme) => 
+
+const useStyles = makeStyles((theme) =>
   createStyles({
-    root:{
+    root: {
       marginTop: theme.spacing(4),
     },
     headContainer: {
-      marginBottom:theme.spacing(3)
-    }
+      
+    },
   })
-)
-
+);
 
 export const Contacts = () => {
   const classes = useStyles();
   const contacts = useContacts();
+  const [dataViewMode, setDataViewMode] = useDataViewMode();
 
-    return (
-      <Container className={classes.root}>
-        <Grid container >
-          <Grid item xs={12}>
-            <Typography variant="h3" component="h1" className={classes.headContainer}>
+  return (
+    <Container className={classes.root}>
+      <Grid container>
+        <Grid item xs={12}>
+          <Box display="flex" justifyContent="space-between" padding="10px">
+            <Typography
+              variant="h4"
+              component="h1"
+              className={classes.headContainer}
+            >
               Contacts
             </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            {(()=>{
-               if(contacts.isLoading) {
-                return <CircularProgress/>;
-              }
-            
-              if(contacts.isError) {
-                return <div>Error...</div>
-              }
-              return <ContactsTable data = {contacts.data}/>
-            })()} 
-          </Grid>
-        </Grid>  
-      </Container>
-    )
-}
+            <ToggleDataViewMode dataViewMode={dataViewMode} setDataViewMode={setDataViewMode}/>
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          {(() => {
+            if (contacts.isLoading) {
+              return <CircularProgress />;
+            }
+
+            if (contacts.isError) {
+              return <div>Error...</div>;
+            }
+            if (dataViewMode === DATA_VIEW_MODES.TABLE) {
+              return <ContactsTable data={contacts.data} />;
+            }
+            if (dataViewMode === DATA_VIEW_MODES.GRID) {
+              return "grid";
+            }
+            return null;
+          })()}
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
